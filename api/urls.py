3ea48ddbb25560b790be2bbeee7cd8f6988c3fef
nartaaboe <kanosadventure@gmail.com>
2025-03-30 22:20:56 +0600

    any

---
+++ api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include 
+
+from rest_framework.routers import DefaultRouter 
+
+from .views import ItemViewSet, RegisterView
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+ 
+
+router = DefaultRouter() 
+
+router.register(r'items', ItemViewSet) 
+
+ 
+
+urlpatterns = [ 
+
+    path('', include(router.urls)), 
+
+    path('register/', RegisterView.as_view(), name='register'), 
+
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+
+] 
